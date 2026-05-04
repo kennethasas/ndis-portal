ddcdae1df2fc91a8cd5516dcc704305014b9289a
Ken Asas <asaswayne@gmail.com>
2026-05-04 12:49:41 +0800

    Update

--- ndis-portal-ui/src/shared/components/icons/svg-icons/sidebar-icon.ts
+++ ndis-portal-ui/src/shared/components/icons/svg-icons/sidebar-icon.ts
@@ -11,7 +11,7 @@ import { Component, Input } from '@angular/core';
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_211_89570)">
+      <g clip-path="url(#clip0_211_88270)">
         <path
           d="M4 6C4 5.46957 4.21071 4.96086 4.58579 4.58579C4.96086 4.21071 5.46957 4 6 4H18C18.5304 4 19.0391 4.21071 19.4142 4.58579C19.7893 4.96086 20 5.46957 20 6V18C20 18.5304 19.7893 19.0391 19.4142 19.4142C19.0391 19.7893 18.5304 20 18 20H6C5.46957 20 4.96086 19.7893 4.58579 19.4142C4.21071 19.0391 4 18.5304 4 18V6Z"
           stroke="currentColor"
@@ -27,7 +27,7 @@ import { Component, Input } from '@angular/core';
           stroke-linejoin="round"
         />
         <path
-          d="M14 10L16 12L14 14"
+          d="M15 10L13 12L15 14"
           stroke="currentColor"
           stroke-width="1.5"
           stroke-linecap="round"
@@ -35,8 +35,8 @@ import { Component, Input } from '@angular/core';
         />
       </g>
       <defs>
-        <clipPath id="clip0_211_89570">
-          <rect width="24" height="24" fill="currentColor" />
+        <clipPath id="clip0_211_88270">
+          <rect width="24" height="24" fill="white" />
         </clipPath>
       </defs>
     </svg>
